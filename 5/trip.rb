9e307e10f433d9b12c5ae503b31459423265a70e
ryuchun00 <leokun0210@gmail.com>
2018-02-11 15:26:44 +0900

    ダックタイピングを盛り込む

--- 5/trip.rb
+++ 5/trip.rb
@@ -1,46 +1,34 @@
-# 問題を悪化させる
-# 旅行の準備はさらに複雑になった
+# 旅行の準備はより簡単になる
 class Trip
   attr_reader :bicycles, :customers, :vehicle
 
   def prepare(prepares)
     preparers.each { |preparer|
-      case preparer
-      when Mechaic
-        preparer.prepare_bicycles(bicycles)
-      when TripCoordinator
-        prepare.buy_food(customers)
-      when Driver
-        preparer.gas_up(cehicle)
-        preparer.fill_water_tank(cehicle)
-      end
+      preparer.prepare_trip(self)
     }
   end
 end
 
+# 全ての準備者(Preparer)は'prepare_trip'に応答するダック
 class Mechanic
-  def prepare_bicycles(bicycles)
-    bicycles.each { |bicycle| prepare_bicycle(bicycle) }
-  end
-
-  def prepare_bicycle(bicycle)
-    # ...
+  def prepare_trip(trip)
+    trip.bicycles.each { |bicycle|
+      prepare_bicycle(bicycle) }
   end
 end
 
-# TripCoordinator と Driver を追加した
 class TripCoordinator
-  def buy_food(customers)
-    # ...
+  def prepare_trip(trip)
+    buy_food(trip.customers)
   end
+
+  # ...
 end
 
 class Driver
-  def gas_up(vehicle)
-    # ...
-  end
-
-  def fill_water_tank(vehicle)
-    # ...
+  def prepare_trip(trip)
+    vehicle = trip.vehicle
+    gas_up(vehicle)
+    fill_water_tank(vehicle)
   end
 end
