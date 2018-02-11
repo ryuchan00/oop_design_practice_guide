e46d0f1fe89f01409315285fa2c53d78b980ec8a
ryuchun00 <leokun0210@gmail.com>
2018-02-11 13:57:36 +0900

    リファクタによりさらに複雑化の一途をたどる

--- 5/trip.rb
+++ 5/trip.rb
@@ -1,21 +1,36 @@
+# 問題を悪化させる
+# 旅行の準備はさらに複雑になった
 class Trip
   attr_reader :bicycles, :customers, :vehicle
 
-  # この'mechanic'引数はどんなクラスのものでも良い。
-  def prepare(mechanic)
-    mechanic.prepare_bicycles(bicycles)
+  def prepare(prepares)
+    preparers.each { |preparer|
+      case preparer
+      when Mechaic
+        preparer.prepare_bicycles(bicycles)
+      when TripCoordinator
+        prepare.buy_food(customers)
+      when Driver
+        preparer.gas_up(cehicle)
+        preparer.fill_water_tank(cehicle)
+      end
+    }
   end
-
-  # ...
 end
 
-# *この*クラスのインスタンスを渡すことになったとしても、動作する
-class Mechanic
-  def prepare_bicycles(bicycles)
-    bicycles.each{ |bicycle| prepare_bicycles(bicycle)}
+# TripCoordinator と Driver を追加した
+class TripCoordinator
+  def buy_food(customers)
+    # ...
+  end
+end
+
+class Driver
+  def gas_up(vehicle)
+    # ...
   end
 
-  def prepare_bicycle(bicycle)
+  def fill_water_tank(vehicle)
     # ...
   end
 end
