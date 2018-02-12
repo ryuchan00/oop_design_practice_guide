33fdf463e7a0dd63bf9d7ae4885abb3b33a3dcaa
ryuchun00 <leokun0210@gmail.com>
2018-02-12 11:28:10 +0900

    styleの判定が開発者の想定していない挙動になる可能性がある

--- 6/bicycle.rb
+++ 6/bicycle.rb
@@ -1,29 +1,43 @@
 # 継承を使うべき箇所を識別する
-# 具象クラスから始める
+# 複数の型を埋め込む
+# 既存のBicycleクラスを変更し、ロードバイク、マウンテンバイクの両方でsparesが動作するということをしています。
 class Bicycle
-  attr_reader :size, :tape_color
+  attr_reader :style, :size, :tape_color, :front_shock, :rear_shock
 
   def initialize(args)
+    @style = args[:style]
     @size = args[:size]
     @tape_color = args[:type_color]
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
   end
 
-  # 全ての自転車は、デフォルト値として、同じタイヤサイズとチェーンサイズをもつ
+  # 'style'の確認は、危険な道へ進む一歩
   def spares
-    {
-      chain: '10-speed',
-      tire_size: '23',
-      tape_color: tape_color
-    }
+    if style == :road
+      {
+        chain: '10-speed',
+        tire_size: '23', # milimeters
+        tape_color: tape_color
+      }
+    else
+      # このelseは、ロードバイク以外を処理するので、マウンテンバイクとは限らない
+      # つまり、想定している動きではなくなる可能性がある
+      {
+        chain: '10-speed',
+        tire_size: '2.1', # inches
+        tape_color: rear_shock
+      }
+    end
   end
-
-  # 他にも多くのメソッドが...
 end
 
 bike = Bicycle.new(
-  size: 'M',
-  tape_color: 'red'
+  style: :mountain,
+  size: 'S',
+  front_shock: 'Manitou',
+  rear_shock: 'Fox'
 )
 
-p bike.size # => M
+p bike.size
 p bike.spares
